b757183338ee79cd5a048826d7e5de0539ebc718
hasanzfdn <hasan.ozfidan@std.yildiz.edu.tr>
2023-05-17 19:32:28 +0300

    coded face and lane functions

--- src/adas_safety_drive/scripts/decision_maker.py
+++ src/adas_safety_drive/scripts/decision_maker.py
@@ -4,12 +4,40 @@ import rospy
 from std_msgs.msg import Bool
 
 
-
+face_alert = False
+lane_alert = False
+cnt = 0
 
 def face(data):
+    # 10 sn'de bir uyku verisi geliyor.
+    # 20 sn' içerisinde üst üste iki veri de uykulu olursa uyarı ver.
+
+    if (data == False):
+        cnt = 0
+
+    else:
+        cnt = cnt + 1
+
+        if (cnt == 2):
+            face_alert = True
+            cnt = 0
 
 
-def lane(data);
+
+
+def lane(data):
+
+    if (data == False):
+        cnt = 0
+
+    else:
+        cnt = cnt + 1
+
+        if (cnt == 2):
+            lane_alert = True
+            cnt = 0
+
+
 
 
 
@@ -18,9 +46,10 @@ def decision_publisher():
 
     rospy.init_node('decision_sub_pub', anonymous=False)
 
-
     rospy.Subscriber('/face_alert', Bool, face)
     rospy.Subscriber('/lane_alert', Bool, lane)
+
+
 
     rospy.spin()
 
