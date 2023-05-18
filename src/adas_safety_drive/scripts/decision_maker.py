2f876c65105b17309e476d8209f4afdc8d979961
hasanzfdn <hasan.ozfidan@std.yildiz.edu.tr>
2023-05-18 19:59:15 +0300

    add time synchronizer

--- src/adas_safety_drive/scripts/decision_maker.py
+++ src/adas_safety_drive/scripts/decision_maker.py
@@ -2,46 +2,67 @@
 
 import rospy
 from std_msgs.msg import Bool
+from std_msgs.msg import string
 
 
 face_alert = False
 lane_alert = False
-cnt = 0
+sleep_publisher = rospy.Publisher('sleep_warning', string, queue_size = 1)
+drive_publisher= rospy.Publisher('drive_warning', string, queue_size = 1)
+
+global face_cnt = 0
+global line_cnt = 0
 
 def face(data):
     # 10 sn'de bir uyku verisi geliyor.
     # 20 sn' içerisinde üst üste iki veri de uykulu olursa uyarı ver.
 
     if (data == False):
-        cnt = 0
+        face_cnt = 0
 
     else:
-        cnt = cnt + 1
+        face_cnt = face_cnt + 1
 
-        if (cnt == 2):
+        if (face_cnt == 1):
+            sleep_publisher.publish('BİR KAHVE MOLASI VERİNİZ')
+
+        if (face_cnt == 2):
             face_alert = True
-            cnt = 0
+            face_cnt = 0
 
+        return face_alert
 
 
 
 def lane(data):
 
     if (data == False):
-        cnt = 0
+        out_lane =+ 1
 
     else:
-        cnt = cnt + 1
-
-        if (cnt == 2):
-            lane_alert = True
-            cnt = 0
+        in_lane =+ 1
 
 
 
+    if (out_lane//in_lane == 0.4):
+        drive_publisher.publish('DİKKATLİ SÜRÜNÜZ')
+        line_cnt =+ 1
 
+    return lane_alert
+'''
+import message_filters
+ from sensor_msgs.msg import Image, CameraInfo
 
+ def callback(image, camera_info):
+    5   # Solve all of perception here...
 
+ image_sub = message_filters.Subscriber('image', Image)
+ info_sub = message_filters.Subscriber('camera_info', CameraInfo)
+
+ ts = message_filters.TimeSynchronizer([image_sub, info_sub], 10)
+ ts.registerCallback(callback)
+ rospy.spin()
+ '''
 def decision_publisher():
 
     rospy.init_node('decision_sub_pub', anonymous=False)
