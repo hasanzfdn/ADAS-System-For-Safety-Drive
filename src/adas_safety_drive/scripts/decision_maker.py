01fb359d606eb233ad727a7c3eda669ffbc7f87b
hasanzfdn <hasan.ozfidan@std.yildiz.edu.tr>
2023-05-19 01:16:50 +0300

    finish decision maker code

--- src/adas_safety_drive/scripts/decision_maker.py
+++ src/adas_safety_drive/scripts/decision_maker.py
@@ -2,18 +2,22 @@
 
 import rospy
 from std_msgs.msg import Bool
-from std_msgs.msg import string
+from std_msgs.msg import String
+import message_filters
+
 
 
 face_alert = False
 lane_alert = False
-sleep_publisher = rospy.Publisher('sleep_warning', string, queue_size = 1)
-drive_publisher= rospy.Publisher('drive_warning', string, queue_size = 1)
-
-global face_cnt = 0
-global line_cnt = 0
+sleep_publisher = rospy.Publisher('sleep_warning', String, queue_size = 1)
+drive_publisher = rospy.Publisher('drive_warning', String, queue_size = 1)
+health_warning_pub = rospy.Publisher('health_situation', Bool, queue_size = 1)
+health_warning = False
+face_cnt = 0
+lane_cnt = 0
 
 def face(data):
+    global face_cnt, face_alert
     # 10 sn'de bir uyku verisi geliyor.
     # 20 sn' içerisinde üst üste iki veri de uykulu olursa uyarı ver.
 
@@ -22,55 +26,62 @@ def face(data):
 
     else:
         face_cnt = face_cnt + 1
-
+        print('dvsdfgvsfdbsrgdsfdgjhdf')
         if (face_cnt == 1):
-            sleep_publisher.publish('BİR KAHVE MOLASI VERİNİZ')
+
+            sleep_publisher.publish('PLEASE TAKE A COFFEE BREAK FOR YOUR HEALTH')
 
         if (face_cnt == 2):
             face_alert = True
             face_cnt = 0
 
-        return face_alert
+    return face_alert
 
 
 
 def lane(data):
+    global lane_cnt, lane_alert
+    # 10 sn'de bir lane verisi geliyor.
+    # 20 sn' içerisinde üst üste iki veri de sıkıntılı olursa uyarı ver.
 
     if (data == False):
-        out_lane =+ 1
+        lane_cnt = 0
 
     else:
-        in_lane =+ 1
+        lane_cnt = lane_cnt + 1
 
+        if (lane_cnt == 1):
+            drive_publisher.publish('PLEASE KEEP YOUR VEHICLE WITHIN THE LANES FOR YOUR HEALTH')
 
-
-    if (out_lane//in_lane == 0.4):
-        drive_publisher.publish('DİKKATLİ SÜRÜNÜZ')
-        line_cnt =+ 1
+        if (lane_cnt == 2):
+            lane_alert = True
+            lane_cnt = 0
 
     return lane_alert
-'''
-import message_filters
- from sensor_msgs.msg import Image, CameraInfo
 
- def callback(image, camera_info):
-    5   # Solve all of perception here...
+def callback(face_sub, lane_sub):
+    global health_warning
+    flag1 = face(face_sub.data)
+    flag2 = lane(lane_sub.data)
 
- image_sub = message_filters.Subscriber('image', Image)
- info_sub = message_filters.Subscriber('camera_info', CameraInfo)
+    if flag1 == True and flag2 == True:
+        health_warning = True
+        health_warning_pub.publish(health_warning)
 
- ts = message_filters.TimeSynchronizer([image_sub, info_sub], 10)
- ts.registerCallback(callback)
- rospy.spin()
- '''
+    else:
+        health_warning_pub.publish(health_warning)
+
+
+
 def decision_publisher():
 
     rospy.init_node('decision_sub_pub', anonymous=False)
 
-    rospy.Subscriber('/face_alert', Bool, face)
-    rospy.Subscriber('/lane_alert', Bool, lane)
+    face_sub = message_filters.Subscriber('/face_alert', Bool)
+    lane_sub = message_filters.Subscriber('/lane_alert', Bool)
 
-
+    ts = message_filters.ApproximateTimeSynchronizer([face_sub, lane_sub], 10, 0.2, allow_headerless=True)
+    ts.registerCallback(callback)
 
     rospy.spin()
 
